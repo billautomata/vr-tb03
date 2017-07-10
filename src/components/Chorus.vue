7ddbb07f9245baf530e90667e21904463e0892a2
Bill McKessy <bill@evident.io>
2017-07-09 18:28:52 -0700

    more knobs on chorus

--- src/components/Chorus.vue
+++ src/components/Chorus.vue
@@ -3,7 +3,15 @@
     <a-box width='1' height='1' depth='0.1' color='#303' position='0.5 -0.5 0'></a-box>
     <a-text value='CHORUS' position='0 -0.75 0.1' rotation='0 0 0' align='left'></a-text>
     <a-entity rotation='0 0 -90'>
-      <a-entity v-on:changed="setWetness" :slider="['initialValue: ', synth.wet.get().value, ';'].join('')" position='0.5 0.5 0.1' scale='0.5 0.5 0.5'></a-entity>
+      <a-entity v-on:changed="setWetness" :slider="['initialValue: ', synth.wet.get().value, ';'].join('')" position='0.5 0.5 0.1' scale='0.5 0.5 0.5'>
+        <a-text value='wetness' rotation='0 0 90' align='center'></a-text>
+      </a-entity>
+      <a-entity v-on:changed="setDepth" :slider="['initialValue: ', synth.depth, ';'].join('')" position='0.3 0.5 0.1' scale='0.5 0.5 0.5'>
+        <a-text value='depth' rotation='0 0 90' align='center'></a-text>
+      </a-entity>
+      <a-entity v-on:changed="setFrequency" :slider="['initialValue: ', scales['frequency'].invert(synth.frequency.value), ';'].join('')" position='0.1 0.5 0.1' scale='0.5 0.5 0.5'>
+        <a-text value='frequency' rotation='0 0 90' align='center'></a-text>
+      </a-entity>
     </a-entity>
   </a-entity>
 </template>
@@ -18,7 +26,24 @@ export default {
       synth: {
         wet: {
           get: function (){
-            return 0
+            return {
+              value: 0
+            }
+          }
+        },
+        depth: {
+          get: function (){
+            return {
+              value: 0
+            }
+          }
+        },
+        feedback: 0,
+        frequency: {
+          get: function (){
+            return {
+              value: 0
+            }
           }
         }
       },
@@ -26,15 +51,19 @@ export default {
     }
   },
   created () {
+    this.scales['frequency'] = new d3.scaleLinear().domain([0.0,1.0]).range([0.0,5.0])
     this.scales['wet'] = new d3.scaleLinear().domain([0.0,1.0]).range([0.0,1.0])
+    this.scales['depth'] = new d3.scaleLinear().domain([0.0,1.0]).range([0.0,1.0])
+    this.scales['feedback'] = d3.scaleLinear().domain([0.0,1.0]).range([0.0,1.0])
   },
   mounted () {
     console.log('chorus mounted')
     var self = this
-    self.synth = new Tone.Chorus(4, 2.5, 0.5)
+    self.synth = new Tone.Chorus(3, 1, 0.5)
     self.synth.channel_name = self.$el.getAttribute('inputChannelName')
     self.$nextTick(function () {
       self.$el.object3D.userData.synth = self.synth
+      console.log('frequency', self.synth.frequency)
       self.synth.receive(self.synth.channel_name)
       EventBus.$emit('new-audio-channel', self.synth)
     })
@@ -43,7 +72,17 @@ export default {
     setWetness: function (event) {
       console.log(event.detail.value)
       this.synth.set('wet', this.scales['wet'](event.detail.value))
-    }
+    },
+    setFrequency: function (event) {
+      console.log(event.detail.value)
+      console.log('set frequency', event.detail.value)
+      this.synth.set('frequency', this.scales['frequency'](event.detail.value))
+    },
+    setDepth: function (event) {
+      console.log(event.detail.value)
+      console.log('set depth', event.detail.value)
+      this.synth.set('depth', this.scales['depth'](event.detail.value))
+    },
   }
 }
 </script>
--- src/components/Distortion.vue
+++ src/components/Distortion.vue
@@ -3,7 +3,7 @@
     <a-box width='1' height='1' depth='0.1' color='#303' position='0.5 -0.5 0'></a-box>
     <a-text value='DISTORTION' position='0 -0.75 0.1' rotation='0 0 0' align='left' scale='0.5 0.5 0.5'></a-text>
     <a-entity rotation='0 0 -90'>
-      <a-entity v-on:changed="setWetness" :slider="['initialValue: ',  synth.wet.get().value, ';'].join('')" position='0.5 0.5 0.1' scale='0.75 0.75 0.75'></a-entity>
+      <a-entity v-on:changed="setWetness" :slider="['initialValue: ',  synth.wet.get().value, ';'].join('')" position='0.1 0.5 0.1' scale='0.75 0.75 0.75'></a-entity>
       <a-entity v-on:changed="setDistortion" :slider="['initialValue: ', scales['distortion'].invert(synth.distortion),';'].join('')" position='0.75 0.5 0.1' scale='0.75 0.75 0.75'></a-entity>
     </a-entity>
   </a-entity>
@@ -29,7 +29,7 @@ export default {
   },
   created () {
     this.scales['wet'] = new d3.scaleLinear().domain([0.0,1.0]).range([0.0,1.0])
-    this.scales['distortion'] = new d3.scaleLinear().domain([0.0,1.0]).range([0.0,30.0])
+    this.scales['distortion'] = new d3.scaleLinear().domain([0.0,1.0]).range([0.0,100.0])
   },
   mounted () {
     console.log('distortion mounted')
