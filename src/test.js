63736ca5220255b53f2f07d3e8e124db5ec9bed8
Bill McKessy <bill@evident.io>
2017-07-17 10:06:19 -0700

    tests

--- src/test.js
+++ src/test.js
@@ -3,9 +3,7 @@ module.exports = function (vueInstance) {
   var Tone = window.Tone
   console.log('test.js loaded')
 
-  console.log(window.filters)
-
-  var o = {
+  var testFilter = {
     frequency: 7777,
     Q: 10,
     type: 'lowpass',
@@ -14,22 +12,9 @@ module.exports = function (vueInstance) {
     outputChannel: 'filterOutput',
     p: { x: 0, y: 0, z: 0 }
   }
-  window.filters.push(o)
+  window.filters.push(testFilter)
 
-  // var filter = document.createElement('filterf')
-  // filter.setAttribute('v-presets', '')
-  // filter.setAttribute('movable', '')
-  // filter.setAttribute('name', 'filter one')
-  // filter.setAttribute('audio-input-channel-connector', 'channel: filter;')
-  // filter.setAttribute('audio-output-channel-selector', 'channel: 0;')
-  //
-  // window.z = filter
-  // document.querySelector('a-entity#testTarget').appendChild(filter)
-  //
-  // setTimeout(function () {
-  //   console.log(document.querySelector('#filter'))
-  // },100)
-
+  // test tone
   var osc = new Tone.Oscillator()
   osc.send('filter')
   osc.set('frequency', 120)
